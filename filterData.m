7aabc9f6f5ed6e7b119a5848f755eef5b665fcdd
Oishe <farhanoishe@gmail.com>
2017-07-27 14:42:44 -0400

    Removing all past files. Working with single file. Full.m has sections inside that run the specific functions

--- filterData.m
+++
@@ -1,8 +0,0 @@
-function [patchFilter] = filterData(patch, fs)
-    fstart = 5;
-    fstop= 55;
-    wstart = 2*pi*fstart/fs;
-    wstop = 2*pi*fstop/fs;
-    b = fir1(4000,[wstart wstop]);
-    patchFilter = filtfilt(b,1,patch);
-end
--- main.m
+++
@@ -1,6 +0,0 @@
-%% Scripts to call in order
-extractData;
-filterAllData;
-% DataFiltered.mat.bak backup from this point
-collectAllEvents;
-% DataCollectedEvents.mat.bak from this point
