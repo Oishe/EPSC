271bdb3ace198be00671a8a910da4442b9748b9a
Oishe <farhanoishe@gmail.com>
2017-08-09 09:52:30 -0400

    mean scatter plot with labels

--- graphType.m
+++ graphType.m
@@ -1,20 +1,49 @@
 load DataCell.mat
-%% graph together
+numOfCells = numel(DataCell);
+%% Plot trace of each cell
 figure;
+meanDecays(numOfCells) = 0;
+meanAmplitudes(numOfCells) = 0;
+meanColors(numOfCells) = 0;
+meanTexts(numOfCells) = 0;
 hold on;
-for cellIdx = 1:numel(DataCell)
+for cellIdx = 1:numOfCells
     if (DataCell{cellIdx}.startSample == -1)
+        meanDecays(cellIdx) = NaN;
+        meanAmplitudes(cellIdx) = NaN;
+        meanColors(cellIdx) = 'k';
+        meanTexts(cellIdx) = cellIdx;
         continue;
     else
         if(DataCell{cellIdx}.Type == 'W')
+            meanColors(cellIdx) ='b';
             colour = sprintf('b');
         elseif(DataCell{cellIdx}.Type == 'K')
+            meanColors(cellIdx) = 'r';
             colour = sprintf('r');
         else
+            meanColors(cellIdx) = 'k';
             colour = sprintf('k');
         end
+        meanDecays(cellIdx) = mean(DataCell{cellIdx}.decays);
+        meanAmplitudes(cellIdx) = mean(DataCell{cellIdx}.amplitudes);
+        meanTexts(cellIdx) = cellIdx;
+        %single trace
         plot(DataCell{cellIdx}.averageEvent, colour);
         text(find(DataCell{cellIdx}.averageEvent==min(DataCell{cellIdx}.averageEvent)), min(DataCell{cellIdx}.averageEvent), num2str(cellIdx));
     end
 end
 hold off
+%% Plot median points
+meanTexts(isnan(meanDecays)) = [];
+meanColors(isnan(meanDecays)) = [];
+meanAmplitudes(isnan(meanDecays)) = [];
+meanDecays(isnan(meanDecays)) = [];
+figure;
+hold on
+scatter(meanDecays, meanAmplitudes, 30, meanColors);
+for textIdx = 1:numel(meanTexts)
+    text(meanDecays(textIdx), meanAmplitudes(textIdx),num2str(meanTexts(textIdx)));
+end
+xlabel('decay');
+ylabel('amplitude');
