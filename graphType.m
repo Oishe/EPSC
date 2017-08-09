67716984cd162b5e984c691dc6073df85abf717b
Oishe <farhanoishe@gmail.com>
2017-08-09 08:54:27 -0400

    no cell toggle

--- graphType.m
+++ graphType.m
@@ -1,21 +1,9 @@
 load DataCell.mat
-%% discard which ones not to graph
-% boolean array: 1 = include, 0 = don't include
-includeCell = ones(numel(DataCell),1);
-%Kynurenic
-includeCell(2) = 0;
-includeCell(9) = 0;
-%Picrotoxin
-% includeCell(4) = 0;
-% includeCell(6) = 0;
-% includeCell(9) = 0;
 %% graph together
 figure;
 hold on;
 for cellIdx = 1:numel(DataCell)
-    if (~includeCell(cellIdx))
-        continue;
-    elseif (DataCell{cellIdx}.startSample == -1)
+    if (DataCell{cellIdx}.startSample == -1)
         continue;
     else
         if(DataCell{cellIdx}.Type == 'W')
